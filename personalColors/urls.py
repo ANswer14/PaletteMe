fefb84f54231174fde4253a3e39172bc8a3ae75e
ANswer14 <dldudgns789789@naver.com>
2026-02-04 11:38:12 +0900

    DOCS: 각종 메서드 주석 추가 - 주석 추가

--- personalColors/urls.py
+++ personalColors/urls.py
@@ -5,5 +5,5 @@ from personalColors import views
 app_name = 'personalColors'
 
 urlpatterns = [
-    path('', views.getColorInfo, name='personalColors'),
+    path('', views.getColorInfo, name='personalColors'), # colrInfo 렌더링 용 path
 ]
--- personalColors/views.py
+++ personalColors/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# colorInfo 렌더링 함수
 def getColorInfo(request):
+
     return render(request, 'personalColors/infoColor.html')
 

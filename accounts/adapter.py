52dcb479c45b118e7b1ea2ab8881ea5fe3f3cf44
rhdqor <rhdqor1015@gmail.com>
2026-02-09 17:20:33 +0900

    FEAT: 구글 로그인으로 회원가입 성공 시 알림 기능 추가

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -5,12 +5,13 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import redirect
 from django.contrib import messages  # 메시지 프레임워크
 from allauth.exceptions import ImmediateHttpResponse
+from allauth.account.adapter import DefaultAccountAdapter
 
 User = get_user_model()
 
-
+# 구글(소셜) 회원가입 클래스
 class MySocialAccountAdapter(DefaultSocialAccountAdapter):
-    def pre_social_login(self, request, sociallogin):
+    def pre_social_login(self, request, sociallogin):  # 가입 전 검사
         # 이미 정상 연결된 구글 계정이면 통과 후 로그인 진행
         if sociallogin.is_existing:
             return
@@ -26,8 +27,7 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
                 # 로그인 페이지로 리다이렉트 (에러 페이지 대신)
                 raise ImmediateHttpResponse(redirect('account_login'))
 
-
-    def populate_user(self, request, sociallogin, data):
+    def populate_user(self, request, sociallogin, data):  # 구글 로그인 시 닉네임 세팅
         """
         닉네임 자동 설정 및 중복 방지
         신규 가입 시 구글 이메일 앞부분을 닉네임에 넣되, 중복 시 랜덤 값을 붙입니다.
@@ -49,4 +49,11 @@ class MySocialAccountAdapter(DefaultSocialAccountAdapter):
                 # 중복이 없다면: 깔끔하게 이메일 아이디 그대로 저장
                 user.nickname = base_nickname
 
+        return user
+
+    def save_user(self, request, sociallogin, form=None):  # 회원가입 완료 메세지 출력
+        user = super().save_user(request, sociallogin, form)
+        # 소셜 회원가입 성공 메시지 예약
+        # 구글에서 준 이름이나 이메일을 활용할 수 있습니다.
+        messages.success(request, "구글 계정으로 회원가입이 완료되었습니다!")
         return user

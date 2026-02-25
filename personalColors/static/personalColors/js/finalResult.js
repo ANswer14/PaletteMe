ab3ee9656837aeda1e858194363d8543db4b344b
juneysh <juneysh83@gmail.com>
2026-02-25 13:37:39 +0900

    STYLE : login 디자인 수정

--- personalColors/static/personalColors/js/finalResult.js
+++ personalColors/static/personalColors/js/finalResult.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         else if (resultText.includes('여름')) resultElement.style.color = '#7FB3D5'; // 여름 대표색
         else if (resultText.includes('가을')) resultElement.style.color = '#B36D3E'; // 가을 대표색
         else if (resultText.includes('겨울')) resultElement.style.color = '#8E44AD'; // 겨울 대표색
+        else if (resultText.includes('뉴트럴')) resultElement.style.color = '#8E8474'; // 뉴트럴 대표색
     }
 
     const pollInterval = setInterval(checkStatus, 1000); // 라이브 프리뷰를 위해 주기를 0.8~1초로 짧게 설정
